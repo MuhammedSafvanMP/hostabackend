b78a8138782aa9dee55acf38a5f10b34390ee6c4
suhaila thasni <suhailathasni0@gmail.com>
2026-06-08 14:06:04 +0530

    updated

--- hospital-service/src/controllers/hospital.controllers.ts
+++ hospital-service/src/controllers/hospital.controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import Hospital from "../models/hospital.model";
