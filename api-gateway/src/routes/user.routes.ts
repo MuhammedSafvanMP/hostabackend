05b71e8d9806b8a52bd075804fd01370e32c34cd
FARHAN <farhan@FARHANs-MacBook-Air.local>
2026-06-18 13:59:27 +0530

    feat (enquiry added)

--- api-gateway/src/routes/user.routes.ts
+++ api-gateway/src/routes/user.routes.ts
@@ -8,6 +8,13 @@ router.use("/users", proxyRequest);
 router.use("/patients", proxyRequest);
 router.use("/vitals", proxyRequest);
 router.use("/prescription", proxyRequest);
+router.use("/documents", proxyRequest);
+router.use("/lab-results", proxyRequest);
+router.use("/email-enquiry", proxyRequest);
+
+
+
+
 
 
 export default router;
